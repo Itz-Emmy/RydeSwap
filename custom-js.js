ce1a9a17d48f98ea7526b8579e48cb25bf0550b8
Emmanuel Agbonyeme <nereus4luv@gmail.com>
2023-05-22 15:55:47 +0100

    updated styles and pages

--- custom-js.js
+++ custom-js.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         labels: ["Seed sale", "Presale", "Liquidity (Pancakeswap)", "RydeSwap Liquidity", "Ecosystem & Marketing", "Unlock", "Airdrop", "Partnerships", "Staking", "1st CEX listing", "2nd CEX listing"],
         datasets: [
           {
-            data: [5, 12, 7.2, 7.2, 10, 8.6, 5, 5, 20, 10, 10],
+            data: [3, 14, 7.2, 7.2, 10, 8.6, 5, 5, 20, 10, 10],
             backgroundColor: [
                 '#ff6384', '#36a2eb', '#ffce56', '#594DAA', '#A2885D', '#1F1AE0', '#756E98', '#AEEFF0', '#E081EA', '#AB9053', '#64549C'
             ],
