7226d1fde6d450ea3861094c8534bee76350736f
Emmanuel Agbonyeme <nereus4luv@gmail.com>
2023-05-24 00:14:03 +0100

    updated homepage

--- custom-js.js
+++ custom-js.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var myChart = new Chart(ctx, {
       type: "doughnut",
       data: {
-        labels: ["Seed sale", "Presale", "Liquidity (Pancakeswap)", "RydeSwap Liquidity", "Ecosystem & Marketing", "Unlock", "Airdrop", "Partnerships", "Staking", "1st CEX listing", "2nd CEX listing"],
+        labels: ["Seed sale", "Presale", "Liquidity (Pancakeswap)", "RydeSwap Liquidity", "Ecosystem & Marketing", "Unlock", "Airdrop", "Partnerships", "Staking", "CEX listings"],
         datasets: [
           {
-            data: [3, 14, 7.2, 7.2, 10, 8.6, 5, 5, 20, 10, 10],
+            data: [3, 14, 7.2, 7.2, 10, 8.6, 5, 5, 20, 20],
             backgroundColor: [
-                '#ff6384', '#36a2eb', '#ffce56', '#594DAA', '#A2885D', '#1F1AE0', '#756E98', '#AEEFF0', '#E081EA', '#AB9053', '#64549C'
+                '#ff6384', '#36a2eb', '#ffce56', '#594DAA', '#A2885D', '#1F1AE0', '#756E98', '#AEEFF0', '#E081EA', '#AB9053'
             ],
             borderColor: [
-                '#ff6384', '#36a2eb', '#ffce56', '#594DAA', '#A2885D', '#1F1AE0', '#756E98', '#AEEFF0', '#E081EA', '#AB9053', '#64549C'
+                '#ff6384', '#36a2eb', '#ffce56', '#594DAA', '#A2885D', '#1F1AE0', '#756E98', '#AEEFF0', '#E081EA', '#AB9053'
             ],
             borderWidth: 1
           }
